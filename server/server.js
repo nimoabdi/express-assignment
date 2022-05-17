9a034d8bbac78e565de01dbdcabbd72a36b333d9
Nimo Abdi <nimoabdi318@gmail.com>
2022-05-17 15:09:53 -0500

    created things

--- server/server.js
+++ server/server.js
@@ -1,1 +1,12 @@
+//how to import express
+const express = require('express');
+// express is a function 
+// create our express app
+const app = express();
+
+// listen for the internet requests
+app.listen(5000, () => {
+console.log('😶‍🌫️ listening')
+});
+
 console.log('in a server no wayyyy');
